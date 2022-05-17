2bb3e7ac503e8fe796f2a599aecc32d11cbbb04e
Archondo <facundo.archondo@gmail.com>
2022-05-17 14:39:40 -0300

    MENSAJE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.module';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-empleados';
+  titulo = 'Listado de Empleados';
+
+  empleados : any[]=[
+    new Empleado("Facundo","Archondo", "Presidente",7500),
+    new Empleado("Ana","Rodano", "Presidenta",7500),
+    new Empleado("Richard","Perez", "Profesor",4500),
+    new Empleado("Raul","Sinaloa", "Jefe",6500),
+  ];
+  cuadroNombre : string = "";
+  cuadroApellido : string = "";
+  cuadroCargo : string = "";
+  cuadroSalario : number = 0;
+  cuadroSalarsio : number = 0;
+  agregarEmpleado(){
+    let nuevoEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo , this.cuadroSalario)
+    this.empleados.push(nuevoEmpleado);
+  }
+
+
 }
